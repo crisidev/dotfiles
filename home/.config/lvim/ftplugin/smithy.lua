e0baafcd2bff031dd95c1947dd7443425bd0a1e5
Bigo <1781140+crisidev@users.noreply.github.com>
2022-05-11 15:48:27 +0100

    Functional smithy_language_server LSP configuration

--- home/.config/lvim/ftplugin/smithy.lua
+++ home/.config/lvim/ftplugin/smithy.lua
@@ -1,38 +1,55 @@
 -- Formatting
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({})
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {}
 
 -- Linting
-local linters = require("lvim.lsp.null-ls.linters")
-linters.setup({})
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {}
 
 -- Lsp config
-vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "smithy-language-server" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "smithy_language_server" })
 
-local configs = require("lspconfig.configs")
+--[[ 
+NOTE: to install and run the smithy_language_server you need to
+$ git clone https://github.com/awslabs/smithy-language-server $HOME/.local/share/nvim/lsp_servers/smithy_language_server
+$ cd $HOME/.local/share/nvim/lsp_servers/smithy_language_server
+$ ./gradlew build
 
--- Check if the config is already defined (useful when reloading this file)
+This will generate the jar with the server implementation.
+]]
+--
+
+local configs = require "lspconfig.configs"
 if not configs.smithy_language_server then
-	configs.smithy_language_server = {
-		default_config = {
-			cmd = { "smithy-language-server" },
-			filetypes = { "smithy" },
-			root_dir = function(fname)
-				return require("lspconfig").util.find_git_ancestor(fname) or vim.fn.getcwd()
-			end,
-			settings = {},
-		},
-	}
+    local home = os.getenv "HOME"
+    configs.smithy_language_server = {
+        default_config = {
+            cmd = {
+                "java",
+                "-jar",
+                home
+                    .. "/.local/share/nvim/lsp_servers/smithy_language_server/build/libs/smithy-language-server-0.0.0-SNAPSHOT.jar",
+                "0",
+            },
+            filetypes = { "smithy" },
+            root_dir = function(fname)
+                return require("lspconfig").util.find_git_ancestor(fname) or vim.fn.getcwd()
+            end,
+            settings = {},
+        },
+    }
 end
-
 
 local status_ok, lsp = pcall(require, "lspconfig")
 if not status_ok then
-	return
+    return
 end
 
-lsp.smithy_language_server.setup({
-	on_attach = require("lvim.lsp").common_on_attach,
-	on_init = require("lvim.lsp").common_on_init,
-	capabilities = require("lvim.lsp").common_capabilities(),
-})
+lsp.smithy_language_server.setup {
+    on_attach = require("lvim.lsp").common_on_attach,
+    on_init = require("lvim.lsp").common_on_init,
+    capabilities = require("lvim.lsp").common_capabilities(),
+    autostart = true,
+}
+
+vim.cmd [[LspStart smithy_language_server]]
