3a14b2eaa03f8bfc2a9c76d045b19a95eeeeee6d
Bigo <1781140+crisidev@users.noreply.github.com>
2022-08-10 19:22:14 +0100

    Remove hacky way to show the language server name for smithy, since mason supports it directly

--- home/.config/lvim/ftplugin/smithy.lua
+++ home/.config/lvim/ftplugin/smithy.lua
@@ -5,5 +5,3 @@ formatters.setup {}
 -- Linting
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {}
-
-require("nvim-lsp-installer._generated.filetype_map")["smithy"] = { "smithy_language_server" }
