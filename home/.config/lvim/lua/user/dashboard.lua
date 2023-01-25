014544d1f3f8b4ccb7e848dbb852e1bcb31f324d
Bigo <1781140+crisidev@users.noreply.github.com>
2023-01-24 16:05:57 -0800

    Fix dashboard

--- home/.config/lvim/lua/user/dashboard.lua
+++ home/.config/lvim/lua/user/dashboard.lua
@@ -75,8 +75,8 @@ M.config = function()
     end
 
     local border_upper =
-        text "╭────────────────────────────╮"
-    local date = text("│  " .. icons.calendar .. "Today is " .. os.date "%a %d %b" .. "     │")
+        text "╭─────────────────────────────╮"
+    local date = text("│  " .. icons.calendar .. "Today is " .. os.date "%a %d %b" .. "      │")
     local nvim_version = text(
         "│  "
             .. " Neovim version "
@@ -85,13 +85,13 @@ M.config = function()
             .. vim.version().minor
             .. "."
             .. vim.version().patch
-            .. "    │"
+            .. "     │"
     )
     local lvim_version =
-        text("│  " .. icons.moon .. " LunarVim " .. require("lvim.utils.git").get_lvim_version() .. " │")
-    local plugin_count = text("│  " .. kind.Module .. plugins .. " plugins in total    │")
+        text("│  " .. icons.moon .. " LunarVim " .. require("lvim.utils.git").get_lvim_version() .. "  │")
+    local plugin_count = text("│  " .. kind.Module .. plugins .. " plugins in total     │")
     local border_lower =
-        text "╰────────────────────────────╯"
+        text "╰─────────────────────────────╯"
 
     local fortune = require "alpha.fortune"()
     -- fortune = fortune:gsub("^%s+", ""):gsub("%s+$", "")
