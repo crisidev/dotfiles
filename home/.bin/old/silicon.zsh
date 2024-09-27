15a9560234b12f18d0b815111694f7b2ffff5e5f
Bigo <1781140+crisidev@users.noreply.github.com>
2024-09-27 22:41:40 +0100

    Remove old binaries

--- home/.bin/old/silicon.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/zsh
-
-if [ $# -ne 4 ]; then
-    echo "usage $0 source-file output-image language resize-percentage"
-    exit 1
-fi
-
-silicon $1 -o $2 --theme Enki-Tokyo-Night --pad-horiz 20 --pad-vert 20 --shadow-blur-radius 20 --background '#00000000' --shadow-color '#00000000' --language $3
-convert -resize $4 $2 $2
